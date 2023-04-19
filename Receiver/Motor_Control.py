ac6eb24c1a6ffb6a1eb3f96b858d62723e38c61c
TastyBrainWave <99412364+TastyBrainWave@users.noreply.github.com>
2023-04-19 13:02:54 +0100

    Create Motor_Control.py

---
+++ Receiver/Motor_Control.py
@@ -0,0 +1,32 @@
+from microbit import *
+from main import motor_pins
+
+f_b = 0
+l_r = 1
+
+def move(_fb, _lr, _speed):
+    op = 0
+    on = 0
+    tp = 0
+    tn = 0
+    if _fb == 'f':
+        op = _speed
+        tp = _speed
+        if _lr == 'l':
+            tp += _speed
+        elif _lr == 'r':
+            op += _speed
+    elif _fb == 'b':
+        on = _speed
+        tn = _speed
+        if _lr == 'l':
+            on += _speed
+        elif _lr == 'r':
+            tn += _speed
+    set_motors(op, on, tp, tn, motor_pins)
+
+def set_motors(one_pos_val, one_neg_val, two_pos_val, two_neg_val, motor_pins):
+    motor_pins[0].write_analog(one_pos_val)
+    motor_pins[1].write_analog(one_neg_val)
+    motor_pins[2].write_analog(two_pos_val)
+    motor_pins[3].write_analog(two_neg_val)
