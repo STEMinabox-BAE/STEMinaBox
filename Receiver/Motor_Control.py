5c542af6f11e01ef870469df6cba6a67f2f1cb8b
TastyBrainWave <99412364+TastyBrainWave@users.noreply.github.com>
2023-05-23 17:22:25 +0100

    Add files via upload

--- Receiver/Motor_Control.py
+++ Receiver/Motor_Control.py
@@ -11,18 +11,18 @@ def move(_fb, _lr, _speed):
         op = _speed
         tp = _speed
         if _lr == 'l':
-            # tp += _speed
-            op += _speed
-        elif _lr == 'r':
-            # op += _speed
             tp += _speed
+        elif _lr == 'r':
+            op += _speed
     elif _fb == 'b':
         on = _speed
         tn = _speed
         if _lr == 'l':
-            on += _speed
-        elif _lr == 'r':
+            # on += _speed
             tn += _speed
+        elif _lr == 'r':
+            # tn += _speed
+            on += _speed
     elif _fb == 's':
         if _lr == 'l':
             tp = _speed
