2b1f674eb1e5723fa882d109895d729685f3f87e
TastyBrainWave <99412364+TastyBrainWave@users.noreply.github.com>
2023-05-23 17:15:58 +0100

    Add files via upload

--- Receiver/main.py
+++ Receiver/main.py
@@ -2,7 +2,7 @@ from microbit import *
 from Motor_Control import *
 from Message_Decoder import *
 
-speed = 512
+speed = 256
 set_radio_channel(1)
 
 f_b_instruction = 0
@@ -13,4 +13,3 @@ while True:
     forward_backward = instructions[f_b_instruction]
     left_right = instructions[l_r_instruction]
     move(forward_backward, left_right, speed)
-    sleep(10)
