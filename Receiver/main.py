3357b407f342b520a0ffff084dcc4f763bfbd7b7
TastyBrainWave <99412364+TastyBrainWave@users.noreply.github.com>
2023-06-07 14:27:57 +0100

    Update main.py

--- Receiver/main.py
+++ Receiver/main.py
@@ -5,11 +5,8 @@ from Message_Decoder import *
 speed = 256
 set_radio_channel(1)
 
-f_b_instruction = 0
-l_r_instruction = 1
-
 while True:
     instructions = get_message()
-    forward_backward = instructions[f_b_instruction]
-    left_right = instructions[l_r_instruction]
+    forward_backward = instructions[0]
+    left_right = instructions[1]
     move(forward_backward, left_right, speed)
