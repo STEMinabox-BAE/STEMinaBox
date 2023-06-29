776c10d7f96837f30c6ad36d3d95d7bc74258f1f
TastyBrainWave <99412364+TastyBrainWave@users.noreply.github.com>
2023-06-29 18:37:25 +0100

    Add files via upload

--- Receiver/main.py
+++ Receiver/main.py
@@ -1,14 +1,23 @@
+# These are instruction manuals for our program to use
 from microbit import *
 from Motor_Control import *
 from Message_Decoder import *
 
-speed = 
-set_radio_channel( )
+# How fast do we want the bus to go?
+speed =
+# What channel is our radio set to?
+set_radio_channel( )        
 
 
-
+# If the bus is on, keep reading this code over and over
 while the_bus_is_on:
+    # Open up the message from the controller and sorts the information
     instructions = open_message()
+    
+    # Look for the forward/backward information
     forward_backward_movement = instructions[are_we_going_forwards_or_backwards]
+    # Look for the left/right information
     left_right_movement = instructions[are_we_going_left_or_right]
+
+    # Tell the bus what you want it to do!
     move(forward_backward_movement, left_right_movement, speed)
